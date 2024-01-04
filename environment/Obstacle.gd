364fe165a341c30a439ad603708d59a51de63cc9
KornyIsDeveloping <132944959+KornyIsDeveloping@users.noreply.github.com>
2024-01-05 00:01:36 +0200

    implemented the script for the score and die action
    When player hits the tree will die but if it pass thorough it scores

--- environment/Obstacle.gd
+++ environment/Obstacle.gd
@@ -2,7 +2,18 @@ extends Node2D
 
 const SPEED = 300
 
+signal score
+
 func _physics_process(delta):
 	position.x += -SPEED * delta
 	if global_position.x <= -200:
-		global_position.x = 500
+		queue_free()
+
+func _on_TreeTrunk_body_entered(body):
+	if body is Player:
+		if body.has_method("die"):
+			body.die()
+
+func _on_ScoreArea_body_exited(body):
+	if body is Player:
+		emit_signal("score")
